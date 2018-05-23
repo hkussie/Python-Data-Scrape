9e1fb13e05c8d9708abfbe229fd54966270d091d
hkussie <hkussie@gmail.com>
2018-05-23 15:34:54 -0700

    Perform more indepth calculations of streamed tweets

---
+++ calculateSentiment.py
@@ -0,0 +1,4 @@
+from textblob import TextBlob
+
+Sentence = TextBlob("Every body is saying Martin has commited a ponzi scheme but the government comittees a ponzi sheme ever day").sentiment
+print(Sentence)
